b79e4c6dc77782721998fb0a00ab71397edae8b0
imaharu <light1200out@gmail.com>
2018-11-19 12:31:35 +0900

    little

--- lesson04/quick_sort.c
+++ lesson04/quick_sort.c
@@ -5,42 +5,71 @@
 #define max_num 20
 
 void show(int array[]){
-	int i;
-	
-	for(i = 0; i < max_num; i++){
-		printf("%d ", array[i]);
-	}
-	printf("\n");
+    int i;
+
+    for(i = 0; i < max_num; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 void swap(int *x, int *y){
-	int tmp;
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+    int tmp;
+    tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void quicksort(int array[], int left, int right){
+    int origin_l = left;
+    int origin_r = right;
+    int pivot = (left + right) / 2;
+    do{
+        while(1){
+            if (array[left] >= array[pivot]){
+                break;
+            }
+            left++;
+        }
+        while(1){
+            if (array[right] <= array[pivot]){
+                break;
+            }
+            right--;
+        }
+        if ( left <= right){
+            swap(&array[left], &array[right]);
+            left++;
+            right--;
+        }
+    }while( left <= right);
+
+    if (origin_l < right){
+        quicksort(array, origin_l, right);
+    }
+    if (left < origin_r){
+        quicksort(array, left , origin_r);
+    }
 }
 
 int main(int argc, char *argv[]){
-	int i, j;
-	int array[20];
-	srand(time(NULL));
+    int i, j;
+    int array[max_num];
+    int left = 0;
+    int right = max_num - 1;
+    srand(time(NULL));
 
-	for(i = 0; i < max_num;i++){
-		array[i] = rand() % 1000 + 1;
-	}
-	printf("交換前\n");
-	show(array);
+    for(i = 0; i < max_num; i++){
+        array[i] = rand() % 1000 + 1;
+    }
 
-	for(i = 0;i < max_num - 1; i++){
-		for(j = i; j < max_num; j++){
-			if(array[i] > array[j]){
-				swap(&array[i], &array[j]);
-			}
-		}
-	}
+    printf("交換前\n");
+    show(array);
 
-	printf("交換後\n");
-	show(array);
+    quicksort(array, left, right);
 
-	return 0;
+    printf("交換後\n");
+    show(array);
+
+    return 0;
 }
