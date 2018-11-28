6dc390f47a09b597c9ce66c21b0c8cc6e220662a
imaharu <light1200out@gmail.com>
2018-11-28 23:48:26 +0900

    Done CSV

--- lesson12_13_14/api.rb
+++ lesson12_13_14/api.rb
@@ -1,5 +1,4 @@
 require 'json'
 puts "Content-Type: application/json\n"
 puts "\n"
-puts 123
 puts JSON.generate({"foo" => "bar"})
--- lesson12_13_14/store_csv_to_sqlite.rb
+++ lesson12_13_14/store_csv_to_sqlite.rb
@@ -1,29 +1,44 @@
 require 'sqlite3'
 require 'csv'
+## Table作成 ##
 db = SQLite3::Database.new("kaken")
+create_table_sql = <<-SQL
+  create table nlp (
+    id integer primary key,
+    name text,
+    type text,
+    keyword text,
+    organ text,
+    start text,
+    finish text
+  );
+SQL
 
-if not File.exists?("kaken")
-    sql = <<-SQL
-      create table nlp (
-        id integer primary key,
-        name text
-        type text,
-        keyword text,
-        organ text,
-        start text
-        end text
-      );
+begin
+    db.execute(create_table_sql)
+rescue SQLite3::SQLException
+    puts "You already created kaken table"
+end
+
+## SQLiteへデータを流し込む##
+CSV.foreach('data.csv', liberal_parsing: true, headers:true).each_with_index do | item, i |
+    name = item["研究代表者"]
+    type = item["研究種目"]
+    keyword = item["キーワード"]
+    organ = item["研究機関"]
+    start = item["研究期間 (年度)"].split.first
+    finish = item["研究期間 (年度)"].split.last
+    insert_data_sql = <<-SQL
+        insert into nlp values(
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        );
     SQL
-    db.execute(sql)
-else
-    puts "You already created kaken database"
+    db.execute(insert_data_sql, i + 1 , name, type, keyword, organ, start, finish)
 end
 db.close
-
-#csv_data = CSV.read('data.csv', 'r:BOM|UTF-8', headers: true)
-CSV.foreach('data.csv', liberal_parsing: true, headers:true).each_with_index do | item, i |
-    puts item["キーワード"]
-    if i == 2
-        break
-    end
-end
